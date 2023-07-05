3736e48a1ed8e48a98fa0cea4748179c4ab1ab84
ruthhawino <awinoruthh@gmail.com>
2023-07-05 16:11:37 +0300

    added apps

--- projects/greenkiosk/cart/admin.py
+++ projects/greenkiosk/cart/admin.py
@@ -1,8 +1,8 @@
-from jango.contrib import admin
+from django.contrib import admin
 
 
 from .models import Cart
 class CartAdmin(admin.ModelAdmin):
-    list_display=("item_id","item_name "," price "," quantity","total_price","date_added","product_image ")
+    list_display=("item_id","item_name","price","quantity","total_price","date_added","product_image")
 admin.site.register(Cart,CartAdmin)
 
--- projects/greenkiosk/greenkiosk/settings.py
+++ projects/greenkiosk/greenkiosk/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'payment',
     'delivery',
     'cart',
+    'reviews',
+    'offers',
+    'receipt',
 ]
 
 MIDDLEWARE = [
